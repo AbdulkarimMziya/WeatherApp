4740289ec31b163e3409374a405c95c0bcac25a7
Abdulkarim Mziya <akmziya@Abdulkarims-MacBook-Air-2.local>
2026-02-21 00:37:01 -0800

    Initial project setup: remove storyboards & reorganize structure

---
+++ WeatherApp/WeatherApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Abdulkarim Mziya on 2026-02-21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
